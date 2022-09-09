cc308a6ade1fc5be71f6baca470036be7f12654b
parthamanohar <110693911+parthamanohar@users.noreply.github.com>
2022-09-09 21:01:33 +0530

    Add files via upload

---
+++ program 2 8th 6 2022.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import pandas as pd
+
+
+# In[18]:
+
+
+pd.read_excel(r'C:\Users\parth\OneDrive\Desktop\DSsession.xlsx')
+
+
+# In[19]:
+
+
+pd.read_csv(r'C:\Users\parth\OneDrive\Desktop\Book2.csv')
+
+
+# In[25]:
+
+
+pd.read_csv(r'C:\Users\parth\OneDrive\Desktop\file1.tsv',sep="\t")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
